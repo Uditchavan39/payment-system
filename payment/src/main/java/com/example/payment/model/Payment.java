4e5c41cc8590d45190386053db09be3d6d2cebeb
udit <uditchavan39@gmail.com>
2026-05-16 13:17:56 +0530

    trigger build

--- payment/src/main/java/com/example/payment/model/Payment.java
+++ payment/src/main/java/com/example/payment/model/Payment.java
@@ -1,12 +1,9 @@
 package com.example.payment.model;
 
-import com.example.payment.common.User;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
